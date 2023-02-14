c5a3b031d3107cc9ba8aec18d5f21e9d42c139da
diegogirao <diegoramiro25@gmail.com>
2023-02-14 07:49:29 -0300

    feat: import do header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
 
 export function App() {
 	return (
 		<div>
+			<Header />
 			<Post
 				author="Joe Doe"
 				content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
