6eea3ea48da060d94f15589ebed6ae592fe36b56
diegogirao <diegoramiro25@gmail.com>
2023-02-15 18:37:54 -0300

    Add imports App.module.css, global.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { Header } from "./components/Header"
 import { Post } from "./Post"
 
+import styles from "./App.module.css"
+
+import "./global.css"
+
 export function App() {
 	return (
 		<div>
 			<Header />
-			<Post
+			{/* <Post
 				author="Joe Doe"
 				content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
 			/>
 			<Post
 				author="Jane Doe"
 				content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
-			/>
+			/> */}
+
+			<div className={styles.wrapper}></div>
 		</div>
 	)
 }
