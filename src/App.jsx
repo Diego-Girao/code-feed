f63d043a9db407bb1d09763fe055596bdf51b13b
diegogirao <diegoramiro25@gmail.com>
2023-02-16 17:03:33 -0300

    Feat import sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
 import { Post } from "./Post"
 
 import styles from "./App.module.css"
@@ -9,19 +10,19 @@ export function App() {
 	return (
 		<div>
 			<Header />
-
-			<div className={styles.wrapper}></div>
-			<aside>sidebar</aside>
-			<main>
-				<Post
-					author="Joe Doe"
-					content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
-				/>
-				<Post
-					author="Jane Doe"
-					content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
-				/>
-			</main>
+			<div className={styles.wrapper}>
+				<Sidebar />
+				<main>
+					<Post
+						author="Joe Doe"
+						content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
+					/>
+					<Post
+						author="Jane Doe"
+						content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
+					/>
+				</main>
+			</div>
 		</div>
 	)
 }
