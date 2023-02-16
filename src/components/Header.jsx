da9e357ecd416a26bcc4da9f0e78d6d706885fe6
diegogirao <diegoramiro25@gmail.com>
2023-02-16 13:12:35 -0300

    Add import logo code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
+import logoCode from "../assets/logo_code.svg"
 import styles from "./Header.module.css"
 
 export function Header() {
 	return (
 		<div className={styles.header}>
-			<h1>Code Feed</h1>
+			<img src={logoCode} alt="logotipo da comunidade code" />
 		</div>
 	)
 }
