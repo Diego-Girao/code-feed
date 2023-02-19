959de94b309c383b9a8d4a366f8c7b94d44237df
diegogirao <diegoramiro25@gmail.com>
2023-02-19 19:05:33 -0300

    Feat criacao da variavel post e exibindo em tela

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,45 @@ import styles from "./App.module.css"
 
 import "./global.css"
 
+const posts = [
+	{
+		id: 1,
+		author: {
+			avatarUrl: "https://github.com/diego-girao.png",
+			name: "Diego Girao",
+			role: "Dev Front-end",
+		},
+		content: [
+			{ type: "paragraph", content: "Faaala 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu repo. É um projeto que fiz usando o CTD. O nome do projeto é BankApp 🚀",
+			},
+			{ type: "link", content: "joe.dev/BankApp" },
+		],
+		publishedAt: new Date("2023-02-20 07:01:00"),
+	},
+	{
+		id: 2,
+		author: {
+			avatarUrl: "https://github.com/pedro-marins.png",
+			name: "Pedro Marins",
+			role: "Mentor @ConquisteSuaVaga",
+		},
+		content: [
+			{ type: "paragraph", content: "Faaala 👋" },
+			{
+				type: "paragraph",
+				content:
+					"Acabei de subir mais um projeto no meu repo. É um projeto que fiz usando o CTD. O nome do projeto é BankApp 🚀",
+			},
+			{ type: "link", content: "joe.dev/BankApp" },
+		],
+		publishedAt: new Date("2023-02-19 09:35:06"),
+	},
+]
+
 export function App() {
 	return (
 		<div>
@@ -13,14 +52,15 @@ export function App() {
 			<div className={styles.wrapper}>
 				<Sidebar />
 				<main>
-					<Post
-						author="Joe Doe"
-						content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
-					/>
-					<Post
-						author="Jane Doe"
-						content="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere aspernatur error, tempora sequi, temporibus possimus consequatur atque laudantium voluptatem consectetur iste itaque, minima ratione officia harum enim magnam dignissimos iure?"
-					/>
+					{posts.map((post) => {
+						return (
+							<Post
+								author={post.author}
+								content={post.content}
+								publishedAt={post.publishedAt}
+							/>
+						)
+					})}
 				</main>
 			</div>
 		</div>
