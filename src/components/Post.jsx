e40bc3c3582e4d6c9b82558ec04b92d1fa1b550a
diegogirao <diegoramiro25@gmail.com>
2023-02-19 20:05:16 -0300

    Feat formatacao da data e destruct do post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,55 @@
+import { format, formatDistanceToNow } from "date-fns"
+import ptBR from "date-fns/locale/pt-BR"
+
 import { Avatar } from "./Avatar"
 import { Comment } from "./Comment"
 import styles from "./Post.module.css"
 
-export function Post(props) {
-	console.log(props)
+export function Post({ author, publishedAt, content }) {
+	const publishedDateFormatted = format(
+		publishedAt,
+		"d 'de' LLL 'às' HH:mm'h'",
+		{
+			locale: ptBR,
+		}
+	)
+
+	const publishedDateToNow = formatDistanceToNow(publishedAt, {
+		locale: ptBR,
+		addSuffix: true,
+	})
+
 	return (
 		<article className={styles.post}>
 			<header>
 				<div className={styles.author}>
-					<Avatar src="https://github.com/diego-girao.png" />
+					<Avatar src={author.avatarUrl} />
 					<div className={styles.authorInfo}>
-						<strong>Joe Doe</strong>
-						<span>Dev Front-end</span>
+						<strong>{author.name}</strong>
+						<span>{author.role}</span>
 					</div>
 				</div>
 
-				<time title="18 de Fevereiro às 13:25h" dateTime="2023-02-18 13:25:42">
-					Publicado há 2h
+				<time
+					title={publishedDateFormatted}
+					dateTime={publishedAt.toISOString()}
+				>
+					{publishedDateToNow}
 				</time>
 			</header>
 
 			<div className={styles.content}></div>
+			{content.map((line) => {
+				if (line.type === "paragraph") {
+					return <p key={line.content}>{line.content}</p>
+				} else if (line.type === "link") {
+					return (
+						<p key={line.content}>
+							<a href="#">{line.content}</a>
+						</p>
+					)
+				}
+			})}
 
 			<form className={styles.commentForm}>
 				<strong>Deixe um feedback</strong>
