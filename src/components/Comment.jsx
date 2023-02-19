0cdca693919ad8dde85c6a241674b89b815a1642
diegogirao <diegoramiro25@gmail.com>
2023-02-19 16:09:30 -0300

    Feat implementando o comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,5 +1,36 @@
+import { ThumbsUp, Trash } from "phosphor-react"
 import styles from "./Comment.module.css"
 
 export function Comment() {
-	return <div className={styles.comment}></div>
+	return (
+		<div className={styles.comment}>
+			<img src="https://github.com/diego-girao.png" alt="" />
+			<div className={styles.commentBox}>
+				<div className={styles.commentContent}>
+					<header>
+						<div className={styles.authorAndTime}>
+							<strong>Joe Doe</strong>
+							<time
+								title="18 de Fevereiro às 13:25h"
+								dateTime="2023-02-18 13:25:42"
+							>
+								Há 2h atrás
+							</time>
+						</div>
+						<button title="Apagar comentário">
+							<Trash size={20} />
+						</button>
+					</header>
+
+					<p>Muito bom Julius, congrats !!! 👏👏 </p>
+				</div>
+				<footer>
+					<button>
+						<ThumbsUp />
+						Aplaudir <span>15</span>
+					</button>
+				</footer>
+			</div>
+		</div>
+	)
 }
